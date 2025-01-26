5ee0f1d38a393a6e6d2ded764cb099ca3b7ff4a1
DerekSturm263 <dereksturm263@gmail.com>
2025-01-25 17:20:17 -0800

    - Created the "Pressure Plate" activator - Created walls, which can disallow the passing of Bubbles or the Player - Created moving platforms, which can be set to wrap, loop, or turn around - Created boulders, which can be trapped by bubbles

--- Bubble Game/Assets/Scripts/Bubble.cs
+++ Bubble Game/Assets/Scripts/Bubble.cs
@@ -1,16 +1,64 @@
 using UnityEngine;
 
+using static BubbleInteractionSettings;
+
+[RequireComponent(typeof(Rigidbody2D), typeof(BoxCollider2D))]
 public class Bubble : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float _despawnTime;
+
+    private void Awake()
     {
-        
+        Destroy(gameObject, _despawnTime);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        foreach (Transform t in GetComponentInChildren<Transform>())
+        {
+            t.position = transform.position;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.TryGetComponent(out BubbleInteractionSettings popSettings))
+        {
+            if (popSettings.Interaction.HasFlag(BubbleInteraction.PopOnTouch))
+            {
+                Destroy(gameObject);
+            }
+
+            if (popSettings.Interaction.HasFlag(BubbleInteraction.BounceOnTouch) && collision.rigidbody.linearVelocityY < 0)
+            {
+                collision.rigidbody.linearVelocityY *= -3;
+            }
+
+            if (popSettings.Interaction.HasFlag(BubbleInteraction.TrapOnTouch))
+            {
+                collision.transform.SetParent(transform, false);
+                collision.transform.localPosition = Vector3.zero;
+
+                collision.rigidbody.bodyType = RigidbodyType2D.Static;
+                collision.collider.enabled = false;
+
+                transform.localScale = new(1.15f, 1.15f);
+            }
+        }
+    }
+
+    private void OnDestroy()
+    {
+        foreach (Rigidbody2D rb in GetComponentsInChildren<Rigidbody2D>())
+        {
+            rb.bodyType = RigidbodyType2D.Dynamic;
+        }
+
+        foreach (Collider2D col in GetComponentsInChildren<Collider2D>())
+        {
+            col.enabled = true;
+        }
+
+        transform.DetachChildren();
     }
 }
--- Bubble Game/Assets/Scripts/PlayerMovement.cs
+++ Bubble Game/Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,83 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(PlayerInput), typeof(SpriteRenderer), typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private SpriteRenderer _sprt;
+    private Rigidbody2D _rb;
+
+    [SerializeField] private float _movementSpeed;
+    [SerializeField] private float _jumpForce;
+
+    [SerializeField] private Vector2 _boxcastOffset;
+    [SerializeField] private Vector2 _boxcastSize;
+    [SerializeField] private LayerMask _boxcastLayer;
+
+    private bool _isGrounded;
+
+    [SerializeField] private GameObject _bubblePrefab;
+    [SerializeField] private int _maxBubbleCount;
+    [SerializeField] private Vector2 _bubbleOffset;
+    [SerializeField] private float _bubbleSpeed;
+    private int _currentBubbleCount;
+
+    private void Awake()
     {
-        
+        _sprt = GetComponent<SpriteRenderer>();
+        _rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        _isGrounded = Physics2D.BoxCast(transform.position + (Vector3)_boxcastOffset, _boxcastSize, 0, Vector2.zero, 0, _boxcastLayer);
+    
+        if (_isGrounded)
+        {
+            _currentBubbleCount = _maxBubbleCount;
+        }
+
+        _sprt.color = _currentBubbleCount > 0 ? Color.blue : Color.red;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position + (Vector3)_boxcastOffset, _boxcastSize);
+    }
+
+    public void Move(InputAction.CallbackContext ctx)
+    {
+        float movement = ctx.ReadValue<Vector2>().x;
+
+        _rb.linearVelocityX = movement * _movementSpeed;
+
+        if (movement < 0)
+            _sprt.flipX = true;
+        else if (movement > 0)
+            _sprt.flipX = false;
+    }
+
+    public void Jump(InputAction.CallbackContext ctx)
+    {
+        if (!_isGrounded)
+            return;
+
+        _rb.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+    }
+
+    public void Bubble(InputAction.CallbackContext ctx)
+    {
+        if (_currentBubbleCount == 0)
+            return;
+
+        --_currentBubbleCount;
+
+        Vector2 bubbleOffset = _bubbleOffset;
+        if (_sprt.flipX)
+            bubbleOffset.x *= -1;
         
+        GameObject bubble = Instantiate(_bubblePrefab, transform.position + (Vector3)bubbleOffset, Quaternion.identity);
+        bubble.GetComponent<Rigidbody2D>().linearVelocity = new Vector2(_bubbleSpeed * (_sprt.flipX ? -1 : 1), 0);
     }
 }
